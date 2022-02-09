19a06cff747cfa74a0dbcc5e897edffb52f5dec3
srgwrtg0342 <99070713+srgwrtg0342@users.noreply.github.com>
2022-02-09 06:03:41 +0200

    Add files via upload

---
+++ player.py
@@ -0,0 +1,50 @@
+import pygame as pg
+import numpy as np
+import math
+
+
+class Player:
+    def __init__(self):
+        self.pos = np.array([0, 0], dtype=float)
+        self.angle = math.pi / 4
+        self.height = 50
+        self.pitch = 50
+        self.angle_vel = 0.01
+        self.vel = 3
+
+    def update(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+
+        pressed_key = pg.key.get_pressed()
+        if pressed_key[pg.K_UP]:
+            self.pitch += self.vel * 4
+        if pressed_key[pg.K_DOWN]:
+            self.pitch -= self.vel * 4
+        if pressed_key[pg.K_f]:
+            self.vel = self.vel * 1.5
+        if pressed_key[pg.K_g]:
+            self.vel = self.vel * 0.5
+
+        if pressed_key[pg.K_LEFT]:
+            self.angle -= self.angle_vel
+        if pressed_key[pg.K_RIGHT]:
+            self.angle += self.angle_vel
+
+        if pressed_key[pg.K_q]:
+            self.height += self.vel
+        if pressed_key[pg.K_e]:
+            self.height -= self.vel
+
+        if pressed_key[pg.K_w]:
+            self.pos[0] += self.vel * cos_a
+            self.pos[1] += self.vel * sin_a
+        if pressed_key[pg.K_s]:
+            self.pos[0] -= self.vel * cos_a
+            self.pos[1] -= self.vel * sin_a
+        if pressed_key[pg.K_a]:
+            self.pos[0] += self.vel * sin_a
+            self.pos[1] -= self.vel * cos_a
+        if pressed_key[pg.K_d]:
+            self.pos[0] -= self.vel * sin_a
+            self.pos[1] += self.vel * cos_a
